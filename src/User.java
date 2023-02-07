2d78ec1481df00799a56e32c6925eb5db24d3ead
Daria Orlova <orlovad1802@gmail.com>
2023-02-07 14:53:24 +0200

    Initial commit

---
+++ src/User.java
@@ -0,0 +1,31 @@
+import java.util.Objects;
+
+public class User {
+    String name;
+    int age;
+    User(String name, int age){
+        this.name=name;
+        this.age=age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age && name.equals(user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
